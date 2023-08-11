bebc9496b357d9918fd2dced1f2e2ee133270ae7
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-08-11 23:54:34 +0600

    setup bottom navigation

--- app/src/main/java/com/example/threadsapp/ProfileActivity.kt
+++ app/src/main/java/com/example/threadsapp/ProfileActivity.kt
@@ -1,11 +1,52 @@
 package com.example.threadsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.btn_home -> {
+                    // TODO navigate to home screen
+                    true
+                }
+
+                R.id.btn_search -> {
+                    // TODO navigate to search screen
+                    true
+                }
+
+                R.id.btn_post -> {
+                    // TODO navigate to post screen
+                    true
+                }
+
+                R.id.btn_like -> {
+                    // TODO navigate to activity screen
+                    true
+                }
+
+                R.id.btn_profile -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
