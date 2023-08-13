32fa0709adcce52674542ebda6d3d5a6f86e99be
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-08-13 17:54:36 +0600

    T2-135 User feeds container design

--- app/src/main/java/com/example/threadsapp/view/home/FollowingFragment.kt
+++ app/src/main/java/com/example/threadsapp/view/home/FollowingFragment.kt
@@ -1,18 +1,20 @@
 package com.example.threadsapp.view.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.threadsapp.R
+import androidx.fragment.app.Fragment
+import com.example.threadsapp.databinding.FragmentFollowing2Binding
 
 class FollowingFragment : Fragment() {
+    private lateinit var binding: FragmentFollowing2Binding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_following2, container, false)
+    ): View {
+        binding = FragmentFollowing2Binding.inflate(inflater, container, false)
+        return binding.root
     }
 }
--- app/src/main/java/com/example/threadsapp/view/home/ForYouFragment.kt
+++ app/src/main/java/com/example/threadsapp/view/home/ForYouFragment.kt
@@ -1,18 +1,20 @@
 package com.example.threadsapp.view.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.threadsapp.R
+import androidx.fragment.app.Fragment
+import com.example.threadsapp.databinding.FragmentForYouBinding
 
 class ForYouFragment : Fragment() {
+    private lateinit var binding: FragmentForYouBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_for_you, container, false)
+    ): View {
+        binding = FragmentForYouBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
