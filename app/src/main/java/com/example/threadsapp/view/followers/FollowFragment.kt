980b371a274dd5b29ab512c729c2a1d425e232c2
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-08-13 16:03:18 +0600

    added sort dialog

--- app/src/main/java/com/example/threadsapp/view/followers/FollowFragment.kt
+++ app/src/main/java/com/example/threadsapp/view/followers/FollowFragment.kt
@@ -1,12 +1,15 @@
 package com.example.threadsapp.view.followers
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.threadsapp.R
 import com.example.threadsapp.adapters.ViewPagerAdapter
 import com.example.threadsapp.databinding.FragmentFollowBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.tabs.TabLayoutMediator
 
 class FollowFragment : Fragment() {
@@ -31,5 +34,46 @@ class FollowFragment : Fragment() {
         }.attach()
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupDialog()
+    }
+
+    private fun setupDialog() {
+        binding.sortBtn.setOnClickListener {
+            showImageOptionsBottomSheet()
+        }
+    }
+
+    @SuppressLint("ResourceType")
+    private fun createBottomSheetDialog(): BottomSheetDialog {
+        val dialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialogStyle)
+        val view = layoutInflater.inflate(R.layout.sort_dialog, null)
+        dialog.setContentView(view)
+        return dialog
+    }
+
+    private fun showImageOptionsBottomSheet() {
+        val dialog = createBottomSheetDialog()
+        val defaultBtn = dialog.findViewById<View>(R.id.btn_default)
+        val latestBtn = dialog.findViewById<View>(R.id.btn_latest)
+        val earliestBtn = dialog.findViewById<View>(R.id.btn_earliest)
+
+
+        defaultBtn?.setOnClickListener {
+            dialog.dismiss()
+            // TODO
+        }
+        latestBtn?.setOnClickListener {
+            // TODO
+            dialog.dismiss()
+        }
+        earliestBtn?.setOnClickListener {
+            // TODO
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
