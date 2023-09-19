88eb0d5d53fc4b3781cbadf802dea0d250e408da
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-09-19 11:27:20 +0600

    T2-30 - Implement a preview for selected media attachments.

--- app/src/main/java/com/example/threadsapp/view/newThread/CreateNewThreadFragment.kt
+++ app/src/main/java/com/example/threadsapp/view/newThread/CreateNewThreadFragment.kt
@@ -1,5 +1,8 @@
 package com.example.threadsapp.view.newThread
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,11 +10,18 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
 import com.example.threadsapp.R
 import com.example.threadsapp.databinding.FragmentCreateNewThreadBinding
+import com.example.threadsapp.util.Utils
 
 class CreateNewThreadFragment : Fragment() {
     private lateinit var binding: FragmentCreateNewThreadBinding
+    private var PICK_IMAGE_REQUEST  = 1
+    private var selectedImageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +33,45 @@ class CreateNewThreadFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        binding.addMedia.setOnClickListener {
+            chooseImage()
+        }
         binding.textReply.setOnClickListener {
             showPopupMenu(it)
         }
+    }
+
+    private fun chooseImage() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            selectedImageUri = data.data
+            Utils.selectedImageUri = selectedImageUri
+            Glide.with(this)
+                .load(selectedImageUri)
+                .override(300, 350)
+                .into(binding.imageView10)
+
+            binding.imageView10.visibility = View.VISIBLE
+        } else {
+            binding.imageView10.visibility = View.GONE
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 
     private fun showPopupMenu(view: View) {
