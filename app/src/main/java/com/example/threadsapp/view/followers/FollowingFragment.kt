44936a1ccdb802f0ac200502747af5780b6d22ff
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-09-27 14:59:17 +0600

    T2-74 - setup following screen

--- app/src/main/java/com/example/threadsapp/view/followers/FollowingFragment.kt
+++ app/src/main/java/com/example/threadsapp/view/followers/FollowingFragment.kt
@@ -1,20 +1,28 @@
 package com.example.threadsapp.view.followers
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.neobis_android_chapter8.viewModels.AuthViewModel.UserInfoViewModel
 import com.example.threadsapp.R
 import com.example.threadsapp.adapters.FollowerAdapter
 import com.example.threadsapp.databinding.FragmentFollowingBinding
 import com.example.threadsapp.model.Follower
+import com.example.threadsapp.viewModel.followViewModel.FollowersViewModel
 
 class FollowingFragment : Fragment() {
     private lateinit var binding: FragmentFollowingBinding
     private lateinit var recyclerView: RecyclerView
+    private val viewModel: FollowersViewModel by viewModels()
+    private val userInfoViewModel: UserInfoViewModel by viewModels()
+    private lateinit var adapter: FollowerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,18 +36,31 @@ class FollowingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val followersList = getFollowersList() // Replace with your list of followers
-
-        val adapter = FollowerAdapter(followersList)
+        adapter = FollowerAdapter(emptyList())
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
+
+        userInfoViewModel.profileData.observe(viewLifecycleOwner) { profile ->
+            if (profile != null) {
+                val userId = profile.pk
+                getListOfFollowers(userId)
+            } else {
+            }
+        }
+        userInfoViewModel.getInfo()
     }
 
-    private fun getFollowersList(): List<Follower> {
-        return listOf(
-            Follower("iamnalimov", "UX/UI", true, false),
-            Follower("lily.rose", "Rosa", true, false),
-            Follower("beautyguru", "Kylie", true, false)
+    @SuppressLint("NotifyDataSetChanged")
+    private fun getListOfFollowers(userId: Int) {
+        viewModel.followersList(
+            userId,
+            onSuccess = { results ->
+                adapter.updateData(results)
+                adapter.notifyDataSetChanged()
+            },
+            onError = {
+                Toast.makeText(requireContext(), "Try Again", Toast.LENGTH_SHORT).show()
+            }
         )
     }
 }
---
+++ app/src/main/java/com/example/threadsapp/viewModel/followViewModel/FollowsViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.threadsapp.viewModel.followViewModel
+
+import androidx.lifecycle.ViewModel
+import com.example.threadsapp.api.RetrofitInstance
+import com.example.threadsapp.model.ProfileModel.FollowerResult
+import com.example.threadsapp.model.ProfileModel.Followers
+import com.example.threadsapp.util.Utils
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class FollowsViewModel : ViewModel() {
+    fun followsList(
+        id: Int,
+        onSuccess: (List<Followers>) -> Unit,
+        onError: (String) -> Unit
+    ) {
+        val apiInterface = RetrofitInstance.followApi
+
+        val token = Utils.token
+        val authHeader = "Bearer $token"
+
+        val call = apiInterface.readFollows(authHeader, id)
+        call.enqueue(object : Callback<FollowerResult> {
+            override fun onResponse(call: Call<FollowerResult>, response: Response<FollowerResult>) {
+                if (response.isSuccessful) {
+                    val results = response.body()
+                    if (results != null) {
+                        onSuccess.invoke(results.results)
+                    }
+                } else {
+                    onError.invoke("Error fetching list of followers")
+                }
+            }
+
+            override fun onFailure(call: Call<FollowerResult>, t: Throwable) {
+                onError.invoke("Network error")
+            }
+        })
+    }
+}
