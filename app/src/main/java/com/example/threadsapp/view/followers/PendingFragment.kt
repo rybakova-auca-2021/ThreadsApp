c17cd8617871cc4a07a9c696cf5ff4ed7b91158e
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-09-27 15:27:47 +0600

    T2-75 - fixed following screen

--- app/src/main/java/com/example/threadsapp/view/followers/PendingFragment.kt
+++ app/src/main/java/com/example/threadsapp/view/followers/PendingFragment.kt
@@ -1,21 +1,26 @@
 package com.example.threadsapp.view.followers
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.threadsapp.R
+import com.example.neobis_android_chapter8.viewModels.AuthViewModel.UserInfoViewModel
 import com.example.threadsapp.adapters.FollowerAdapter
-import com.example.threadsapp.databinding.FragmentFollowingBinding
 import com.example.threadsapp.databinding.FragmentPendingBinding
-import com.example.threadsapp.model.Follower
+import com.example.threadsapp.viewModel.followViewModel.FollowersViewModel
+import com.example.threadsapp.viewModel.followViewModel.PendingViewModel
 
 class PendingFragment : Fragment() {
     private lateinit var binding: FragmentPendingBinding
     private lateinit var recyclerView: RecyclerView
+    private val viewModel: PendingViewModel by viewModels()
+    private lateinit var adapter: FollowerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,18 +34,23 @@ class PendingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val followersList = getFollowersList()
-
-        val adapter = FollowerAdapter(followersList)
+        adapter = FollowerAdapter(emptyList())
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
+
+        getListOfFollowRequests()
     }
 
-    private fun getFollowersList(): List<Follower> {
-        return listOf(
-            Follower("iamnalimov", "UX/UI", false, true),
-            Follower("lily.rose", "Rosa", false, true),
-            Follower("beautyguru", "Kylie", false, true)
+    @SuppressLint("NotifyDataSetChanged")
+    private fun getListOfFollowRequests() {
+        viewModel.pendingList(
+            onSuccess = { results ->
+                adapter.updateData(results)
+                adapter.notifyDataSetChanged()
+            },
+            onError = {
+                Toast.makeText(requireContext(), "Try Again", Toast.LENGTH_SHORT).show()
+            }
         )
     }
 }
