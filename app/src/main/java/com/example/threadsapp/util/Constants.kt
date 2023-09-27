14f964ff46787c4bdbadf436b1435d31be80f5d9
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-09-27 17:55:24 +0600

    changed url

--- app/src/main/java/com/example/threadsapp/util/Constants.kt
+++ app/src/main/java/com/example/threadsapp/util/Constants.kt
@@ -2,6 +2,6 @@ package com.example.threadsapp.util
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://18.184.167.19:8000/"
+        const val BASE_URL = "https://pavel-backender.org.kg/"
     }
 }
